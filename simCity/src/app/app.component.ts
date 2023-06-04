641746b3c4c967a82529fdfaae5281a7969c5c45
Angel Cantalejo <acantal3@corp.vodafone.es>
2023-06-04 21:37:55 +0200

    Diseño final

--- simCity/src/app/app.component.ts
+++ simCity/src/app/app.component.ts
@@ -8,15 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   public showbutton: boolean = false;
+  public imgToShow: string = '';
 
   ngOnInit(): void {
-    // codigo que se ejecuta nada mas iniciar el componente
+    let svgElement = document.querySelector(".logoImg svg");
+    if (svgElement) {
+      svgElement.remove();
+    }
   }
-  public showElement(stateButton: boolean): void {
+  public showElement(stateButton: boolean, type: string): void {
     if (stateButton === true) {
       this.showbutton = false;
     } else {
       this.showbutton = true;
+    }
+    if (type === 'biodiversity') {
+      this.imgToShow = "../assets/Imagenes/BioDiversity.png";
+    } else if (type === 'climate') {
+      this.imgToShow = "../assets/Imagenes/ClimateAction.png";
+    } else if (type === 'compilance') {
+      this.imgToShow = "../assets/Imagenes/CompilanceDash.png";
+    } else if (type === 'susta') {
+      this.imgToShow = "../assets/Imagenes/Sustan.png";
     }
   }
 }
